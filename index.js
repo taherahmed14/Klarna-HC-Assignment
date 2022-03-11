89c2569d1b2ed17121a1480aa25c5b0c14ad7462
taherahmed14 <taherahmed.sj.95@gmail.com>
2022-03-11 18:18:11 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const connect = require("./config/db");
+const path = require('path');
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// const blogController = require("./controller/blogCont");
+
+// app.use("/blog", blogController);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+});
+
+app.listen(PORT, async () => {
+    await connect();
+    console.log("Listening on Port 5000");
+});
